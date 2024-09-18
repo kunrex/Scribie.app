ce11c782506f9e5cb3a29532c85a403a5a89fc3b
kunrex <74914319+kunrex@users.noreply.github.com>
2024-09-18 17:44:47 +0530

    small change

--- src/app/services/sound/sound-affect-service.ts
+++ src/app/services/sound/sound-affect-service.ts
@@ -18,20 +18,35 @@ export class SoundService extends Initilaisiable {
 
     const keys = Object.keys(SoundEnum);
     const values = Object.values(SoundEnum);
-    for(var i: number = 0; i < values.length; i++) {
-      await NativeAudio.preload({
-        assetId: keys[i],
-        assetPath: values[i] + ".mp3",
 
-        isUrl: false,
-        audioChannelNum: 1
-      });
+    try {
+      for(var i: number = 0; i < values.length; i++) {
+        await NativeAudio.preload({
+          assetId: keys[i],
+          assetPath: values[i] + ".mp3",
+  
+          isUrl: false,
+          audioChannelNum: 1
+        });
+      }
+
+      this.initialise();
     }
-
-    this.initialise();
+    catch(e) {
+      if(typeof e === "string") {
+        console.log("ERROR DURING AUDIO READ:" + e);
+      }
+      else if(e instanceof Error) {
+        console.log("ERROR DURING AUDIO READ:" + e.message);
+      }
+    }
   }
 
   async playSound(sound: SoundEnum) : Promise<void> {
+    if(!this.IsInitialised()) {
+      return;
+    }
+    
     await NativeAudio.play({
       assetId: sound,
       time: 0
